91d88b1b89f393312f21edeb1ba0845b180063a8
Shuang Wu <shuang.wu@emory.edu>
2021-12-26 02:32:38 -0500

    Create 28. Implement strStr().py

---
+++ 28. Implement strStr().py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if haystack == "" and needle == "":
+            return 0
+        if len(needle) > len(haystack):
+            return -1
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
