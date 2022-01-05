e192744e0c9ee4ccb23c29c3209308c6ca37d11b
Shuang Wu <shuang.wu@emory.edu>
2022-01-05 14:08:44 -0500

    Create 509. Fibonacci Number.py

---
+++ 509. Fibonacci Number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0 or n == 1:
+            return n
+        a = 0
+        b = 1
+        for i in range(n-1):
+            c = a+b
+            a = b
+            b = c
+        return c
