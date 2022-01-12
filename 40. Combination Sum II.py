e9e51ddc760d1e3ced4efe0e59e3fd2a41aecbc3
Shuang Wu <shuang.wu@emory.edu>
2022-01-11 22:56:42 -0500

    Update 40. Combination Sum II.py

--- 40. Combination Sum II.py
+++ 40. Combination Sum II.py
@@ -13,11 +13,10 @@ class Solution(object):
             if target < 0:
                 return
             if target == 0:
-                if path not in result:
-                    result.append(path)
+                result.append(path)
                 return 
             for i in range(index, len(candidates)):
-                if i > index and candidates[index - 1] == candidates[index]:
+                if i > index and candidates[i - 1] == candidates[i]:
                     continue
                 helper(i+1, target - candidates[i], path+[candidates[i]], result)
         result = []
