8afe2886964acc796cde4fe0d8e16da9419c9c78
Shuang Wu <shuang.wu@emory.edu>
2022-01-15 21:15:41 -0500

    Update 849. Maximize Distance to Closest Person.py

--- 849. Maximize Distance to Closest Person.py
+++ 849. Maximize Distance to Closest Person.py
@@ -4,20 +4,13 @@ class Solution(object):
         :type seats: List[int]
         :rtype: int
         """
-        ans = -1
-        left = -1
+        ans = p = -1
         for i in range(len(seats)):
             if seats[i] == 1:
-                right = i
-                if left == -1:
-                    closest = right-left-1
-                    ans = max(closest,ans)
-                    left = right
+                if p == -1:
+                    ans = max(i-1-p,ans)
                 else:
-                    closest = (right-left) // 2
-                    ans = max(closest,ans)
-                    left = right
-            if i == len(seats) - 1:
-                closest = i - right
-                ans = max(closest,ans)
+                    ans = max((i-p) // 2,ans)
+                p = i
+        ans = max(len(seats) - 1 - p,ans)
         return ans
