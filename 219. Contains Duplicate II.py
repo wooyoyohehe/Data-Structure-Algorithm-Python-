bcd24c45bb51f4e0803ca4925cec9af147c4ebdc
Shuang Wu <shuang.wu@emory.edu>
2022-01-17 19:09:11 -0500

    Update 219. Contains Duplicate II.py

--- 219. Contains Duplicate II.py
+++ 219. Contains Duplicate II.py
@@ -9,6 +9,5 @@ class Solution(object):
         for i in range(len(nums)):
             if nums[i] in dic and i-dic[nums[i]] <= k:
                 return True
-            else:
-                dic[nums[i]] = i 
+            dic[nums[i]] = i 
         return False 
