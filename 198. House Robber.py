9447b53747650f5dfcaeda525b966966397c0947
Shuang Wu <shuang.wu@emory.edu>
2022-01-17 21:03:49 -0500

    Create 198. House Robber.py

---
+++ 198. House Robber.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 1:
+            return nums[0]
+        nums[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            nums[i] = max(nums[i-2]+nums[i], nums[i-1])
+        return nums[-1]
