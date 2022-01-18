df9f404b5375e5028fe6ce01dcd369a3bec86747
Shuang Wu <shuang.wu@emory.edu>
2022-01-17 21:15:23 -0500

    Update 198. House Robber.py

--- 198. House Robber.py
+++ 198. House Robber.py
@@ -4,9 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) == 1:
-            return nums[0]
-        nums[1] = max(nums[0], nums[1])
-        for i in range(2, len(nums)):
-            nums[i] = max(nums[i-2]+nums[i], nums[i-1])
+        if len(nums) != 1:
+            nums[1] = max(nums[0], nums[1])
+            for i in range(2, len(nums)):
+                nums[i] = max(nums[i-2]+nums[i], nums[i-1])
         return nums[-1]
