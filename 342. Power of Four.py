328f100e9f3a4f8352a4bd2fa44eb8fd76564739
Shuang Wu <shuang.wu@emory.edu>
2022-01-20 23:52:59 -0500

    Create 342. Power of Four.py

---
+++ 342. Power of Four.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        while n!=1:
+            if n % 4 != 0:
+                return False
+            n /= 4
+        return True
+                
