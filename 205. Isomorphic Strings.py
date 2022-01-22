910784de62a2037e06f2e5e4921330538bbf0aa2
Shuang Wu <shuang.wu@emory.edu>
2022-01-22 14:17:55 -0500

    Create 205. Isomorphic Strings.py

---
+++ 205. Isomorphic Strings.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        dic = {}
+        for i in range(len(s)):
+            if s[i] not in dic:
+                if t[i] in dic.values():
+                    return False
+                else:
+                    dic[s[i]] = t[i]
+            elif dic[s[i]] != t[i]:
+                return False
+        return True
