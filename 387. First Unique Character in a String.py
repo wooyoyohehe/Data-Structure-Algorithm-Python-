b34fc1e221809cf7cde586981b05b4e0ac30657e
Shuang Wu <shuang.wu@emory.edu>
2022-01-22 17:00:13 -0500

    Create 387. First Unique Character in a String.py

---
+++ 387. First Unique Character in a String.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dic = {}
+        for i in range(len(s)):
+            if s[i] not in dic:
+                dic[s[i]] = 1
+            else:
+                dic[s[i]] += 1
+        for i in range(len(s)):
+            if dic[s[i]] == 1:
+                return i
+        return -1
