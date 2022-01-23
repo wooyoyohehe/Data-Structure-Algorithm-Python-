19caa7e74c610b4c182285c0b11ab6547c590e78
Shuang Wu <shuang.wu@emory.edu>
2022-01-22 23:10:19 -0500

    Update 746. Min Cost Climbing Stairs.py

--- 746. Min Cost Climbing Stairs.py
+++ 746. Min Cost Climbing Stairs.py
@@ -1,12 +1,20 @@
 class Solution(object):
-    def minCostClimbingStairs(self, cost):
+    def rob(self, nums):
         """
-        :type cost: List[int]
+        :type nums: List[int]
         :rtype: int
         """
-        dp = [0] * len(cost)
-        dp[0] = cost[0]
-        dp[1] = cost[1]
-        for i in range(2,len(cost)):
-            dp[i] = min(dp[i-1], dp[i-2])+cost[i]
-        return min(dp[-1], dp[-2])
+        if len(nums) == 1:
+            return max(nums)
+        dp = [0] * len(nums)
+        dp[1] = nums[1]
+        for i in range(2,len(nums)):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2,len(nums)):
+            if i == len(nums)-1:
+                dp[i] = max(dp[i-1],dp[i-2],dp[i])
+                break
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        return dp[-1]
