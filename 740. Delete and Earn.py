e0aa718f58459e1132370838c6112a2e9ee72baf
Shuang Wu <shuang.wu@emory.edu>
2022-01-23 16:16:55 -0500

    Update 740. Delete and Earn.py

--- 740. Delete and Earn.py
+++ 740. Delete and Earn.py
@@ -9,7 +9,6 @@ class Solution(object):
         hash_table = [0] * (max(nums)+1)
         for i in range(len(nums)):
             hash_table[nums[i]] += nums[i]
-        hash_table[2] = max(hash_table[2],hash_table[1])
-        for i in range(3, len(hash_table)):
+        for i in range(2, len(hash_table)):
             hash_table[i] = max(hash_table[i-2]+hash_table[i], hash_table[i-1])
         return max(hash_table[-1],hash_table[-2])
