3054c3d7661038a9b1547b033b783ab19d3c598f
Shuang Wu <shuang.wu@emory.edu>
2022-01-22 21:52:19 -0500

    Create 746. Min Cost Climbing Stairs.py

---
+++ 746. Min Cost Climbing Stairs.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        dp = [0] * len(cost)
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        for i in range(2,len(cost)):
+            dp[i] = min(dp[i-1], dp[i-2])+cost[i]
+        return min(dp[-1], dp[-2])
