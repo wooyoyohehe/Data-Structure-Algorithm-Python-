2b80726929099930cb5bbd18ccd4e2c1e1e20b20
Shuang Wu <shuang.wu@emory.edu>
2022-02-02 18:31:45 -0500

    Create 300. Longest Increasing Subsequence.py

---
+++ 300. Longest Increasing Subsequence.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = [1]*len(nums)
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
