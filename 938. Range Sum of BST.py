1a19898bf8564388335d11e6abad5082648fecaf
Shuang Wu <shuang.wu@emory.edu>
2022-02-04 20:47:45 -0500

    Create 938. Range Sum of BST.py

---
+++ 938. Range Sum of BST.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rangeSumBST(self, root, low, high):
+        """
+        :type root: TreeNode
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        ans = [0]
+        def helper(node):
+            if not node:
+                return 
+            if low<=node.val<=high:
+                ans[0] += node.val
+            if node.val > low:
+                helper(node.left)
+            if node.val < high:
+                helper(node.right)
+        helper(root)
+        return ans[0]
