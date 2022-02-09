c04d17bcf02848771bd75addb01b319a29156c79
Shuang Wu <shuang.wu@emory.edu>
2022-02-08 23:06:33 -0500

    Create 513. Find Bottom Left Tree Value.py

---
+++ 513. Find Bottom Left Tree Value.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def findBottomLeftValue(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        level = [root]
+        while len(level)>0:
+            temp = []
+            for node in level:
+                if node.left:
+                    temp.append(node.left)
+                if node.right:
+                    temp.append(node.right)
+            if len(temp) == 0:
+                return level[0].val
+            else:
+                level = temp
