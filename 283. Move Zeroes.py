2ebbd7a617d2bf9eed3d3043649d9dd8443f2527
Shuang Wu <shuang.wu@emory.edu>
2022-02-12 21:53:38 -0500

    Create 283. Move Zeroes.py

---
+++ 283. Move Zeroes.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) == 1:
+            return nums
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0 and nums[slow] == 0:
+                nums[fast], nums[slow] = nums[slow], nums[fast]
+            if nums[slow] != 0:
+                slow += 1
