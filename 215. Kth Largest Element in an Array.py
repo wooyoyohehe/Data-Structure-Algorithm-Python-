d3ace5643eb98d522f0075504a84dffac65ee1a9
Shuang Wu <shuang.wu@emory.edu>
2022-02-14 10:22:42 -0500

    Update 215. Kth Largest Element in an Array.py

--- 215. Kth Largest Element in an Array.py
+++ 215. Kth Largest Element in an Array.py
@@ -1,3 +1,4 @@
+import heapq
 class Solution(object):
     def findKthLargest(self, nums, k):
         """
@@ -5,15 +6,11 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        count = 0
-        heap = []
-        for hehe in nums:
-            if count >= k:
-                heapq.heappush(heap, hehe)
-                heapq.heappop(heap)
+        
+        q = []
+        for num in nums:
+            if len(q) == k:
+                heapq.heappushpop(q, num)
             else:
-                heapq.heappush(heap, hehe)
-                count += 1
-        return heap[0]
-
-  
+                heapq.heappush(q, num)
+        return heapq.heappop(q)
