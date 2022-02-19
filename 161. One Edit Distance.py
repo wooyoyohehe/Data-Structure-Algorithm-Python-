a4eb2390047325b14c29deeef8668681dbc4c014
Shuang Wu <shuang.wu@emory.edu>
2022-02-18 22:00:04 -0500

    Create 161. One Edit Distance.py

---
+++ 161. One Edit Distance.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def isOneEditDistance(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if abs(len(s)-len(t)) >= 2 or s == t:
+            return False
+        i = 0
+        j = 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            else:
+                if len(s) == len(t):
+                    if s[i+1:] == t[j+1:]:
+                        return True
+                    else:
+                        return False
+                if len(s) > len(t):
+                    if s[i+1:] == t[j:]:
+                        return True
+                    else:
+                        return False
+                else:
+                    if s[i:] == t[j+1:]:
+                        return True
+                    else:
+                        return False
+        return True
