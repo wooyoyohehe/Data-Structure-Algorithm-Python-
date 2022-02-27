c8865959295d4f9917dbc64f251e1290c65e4775
Shuang Wu <shuang.wu@emory.edu>
2022-02-27 16:10:39 -0500

    Update 540. Single Element in a Sorted Array.py

--- 540. Single Element in a Sorted Array.py
+++ 540. Single Element in a Sorted Array.py
@@ -7,16 +7,16 @@ class Solution(object):
         left, right = 0, len(nums)-1
         while left < right:
             mid = left + (right-left) // 2
-            if nums[mid] != nums[mid-1] and nums[mid] == nums[mid+1]:
-                if mid % 2 == 0:
-                    left = mid + 1
-                else:
-                    right = mid-1
-            elif nums[mid] == nums[mid-1] and nums[mid] != nums[mid+1]:
-                if mid % 2 == 0:
-                    right = mid-1
-                else:
-                    left = mid + 1
-            else:
+            if nums[mid] != nums[mid-1] and nums[mid] != nums[mid+1]:
                 return nums[mid]
+            elif nums[mid] != nums[mid-1]:
+                if mid % 2 == 0:
+                    left = mid + 1
+                else:
+                    right = mid-1
+            else:
+                if mid % 2 == 0:
+                    right = mid-1
+                else:
+                    left = mid + 1
         return nums[left]
