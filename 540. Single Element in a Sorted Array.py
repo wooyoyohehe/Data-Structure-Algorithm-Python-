2a58fabe425b8158b77f0752d704e56c9d31d4c8
Shuang Wu <shuang.wu@emory.edu>
2022-02-27 16:08:15 -0500

    Create 540. Single Element in a Sorted Array.py

---
+++ 540. Single Element in a Sorted Array.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def singleNonDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = left + (right-left) // 2
+            if nums[mid] != nums[mid-1] and nums[mid] == nums[mid+1]:
+                if mid % 2 == 0:
+                    left = mid + 1
+                else:
+                    right = mid-1
+            elif nums[mid] == nums[mid-1] and nums[mid] != nums[mid+1]:
+                if mid % 2 == 0:
+                    right = mid-1
+                else:
+                    left = mid + 1
+            else:
+                return nums[mid]
+        return nums[left]
