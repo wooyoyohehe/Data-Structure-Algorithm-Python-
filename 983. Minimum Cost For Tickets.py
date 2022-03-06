277421b46c11ffb30b20f8a927a5129247d43e87
Shuang Wu <shuang.wu@emory.edu>
2022-03-06 15:26:27 -0500

    Update 983. Minimum Cost For Tickets.py

--- 983. Minimum Cost For Tickets.py
+++ 983. Minimum Cost For Tickets.py
@@ -5,18 +5,13 @@ class Solution(object):
         :type costs: List[int]
         :rtype: int
         """
-        # day = 20
-        # dp[20] = min[2, 7, 15] = 2
-        # dp[8] = min[4, 9, 15] = 4
-        # dp[7] = min[6, 9, 15] = 6
-        # dp[6] = min[8, 9, 15] = 8
-        # dp[4] = min[10, 9, 17] = 9
-        # dp[1] = min[12, 11, 17] = 11
         dp = [0 for _ in range(days[-1]+1)]
+        j = 0
         for i in range(1, days[-1]+1):
-            if i not in days:
+            if i != days[j]:
                 dp[i] = dp[i-1]
             else:
+                j+=1
                 one = costs[0]+dp[i-1] if i>=1 else costs[0]
                 seven = costs[1]+dp[i-7] if i>=7 else costs[1]
                 thirty = costs[2]+dp[i-30] if i>=30 else costs[2]
