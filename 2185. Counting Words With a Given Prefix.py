ebfe0b34f4bccc843f948d4963fbc27d2f799488
Shuang Wu <shuang.wu@emory.edu>
2022-03-10 21:15:08 -0500

    Create 2185. Counting Words With a Given Prefix.py

---
+++ 2185. Counting Words With a Given Prefix.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def prefixCount(self, words, pref):
+        """
+        :type words: List[str]
+        :type pref: str
+        :rtype: int
+        """
+        ans = 0
+        for word in words:
+            if word[:len(pref)] == pref:
+                ans += 1
+        return ans
