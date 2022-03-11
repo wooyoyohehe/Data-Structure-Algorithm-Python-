d4c667c3e8c677706173092de426ec4e2ad5e063
Shuang Wu <shuang.wu@emory.edu>
2022-03-11 16:59:29 -0500

    Create 1302. Deepest Leaves Sum.py

---
+++ 1302. Deepest Leaves Sum.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def deepestLeavesSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        level = [root]
+        while level:
+            temp = []
+            for node in level:
+                if node.left:
+                    temp.append(node.left)
+                if node.right:
+                    temp.append(node.right)
+            pre_level = level
+            level = temp
+        ans = 0
+        for node in pre_level:
+            ans += node.val
+        return ans
+        
