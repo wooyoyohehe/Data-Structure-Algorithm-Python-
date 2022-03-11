9f47a0d797a1853319a7bdb3da10b2ddae47d004
Shuang Wu <shuang.wu@emory.edu>
2022-03-11 17:17:13 -0500

    Create 2176. Count Equal and Divisible Pairs in an Array.py

---
+++ 2176. Count Equal and Divisible Pairs in an Array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def countPairs(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        ans = 0
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j] and i * j % k == 0:
+                    ans += 1
+        return ans
