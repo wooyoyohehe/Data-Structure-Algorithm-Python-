f1354d601e0549f431554b6236abd3f8f6dcfac3
Shuang Wu <shuang.wu@emory.edu>
2022-03-12 07:25:50 -0500

    Create 1304. Find N Unique Integers Sum up to Zero.py

---
+++ 1304. Find N Unique Integers Sum up to Zero.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def sumZero(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        ans = []
+        for i in range(1, n//2+1):
+            ans.append(i)
+            ans.append(-i)
+        if n % 2 == 1:
+            ans.append(0)
+        return ans
