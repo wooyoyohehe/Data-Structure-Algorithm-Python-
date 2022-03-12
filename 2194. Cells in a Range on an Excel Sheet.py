638ccf8fd0ebcc0b00b3557d3c8ace5b0918dcd0
Shuang Wu <shuang.wu@emory.edu>
2022-03-12 10:30:06 -0500

    Update 2194. Cells in a Range on an Excel Sheet.py

--- 2194. Cells in a Range on an Excel Sheet.py
+++ 2194. Cells in a Range on an Excel Sheet.py
@@ -8,6 +8,5 @@ class Solution(object):
         cols = ord(s[3])-ord(s[0])+1
         for i in range(cols):
             for row in range(int(s[1]), int(s[4])+1):
-                letter = chr(ord(s[0])+i)
                 ans.append(chr(ord(s[0])+i)+str(row))
         return ans
