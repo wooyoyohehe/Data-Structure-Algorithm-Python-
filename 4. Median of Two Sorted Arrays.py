5ed01425acd2c987dfb647739da392c6a23bb84b
Shuang Wu <shuang.wu@emory.edu>
2022-04-10 17:31:52 -0400

    Update 4. Median of Two Sorted Arrays.py

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -5,9 +5,45 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        nums = nums1+nums2
-        nums.sort()
-        if len(nums) % 2 == 1:
-            return nums[len(nums)//2]
+        """
+        1,2
+        |
+        3,4
+        |
+        
+        """
+        
+        i,j = 0,0
+        length = len(nums1)+len(nums2)
+        median1 = 0
+        median2 = 0
+        
+        while i+j<=length//2:
+            if i < len(nums1) and (j == len(nums2) or nums1[i] <= nums2[j]):
+                i += 1
+                if i+j == length//2:
+                    median1 = nums1[i-1]
+                if i+j == length//2+1:
+                    median2 = nums1[i-1]
+                    break
+            elif j < len(nums2) and (i == len(nums1) or nums1[i] > nums2[j]):
+                j += 1
+                if i+j == length//2:
+                    median1 = nums2[j-1]
+                if i+j == length//2+1:
+                    median2 = nums2[j-1]
+                    break
+        if length%2:
+            return median2
         else:
-            return float(nums[len(nums)//2] + nums[len(nums)//2 - 1]) / 2
+            return (median1+median2)/2.0
+                    
+            
+
+
+                
+            
+                    
+            
+            
+        
