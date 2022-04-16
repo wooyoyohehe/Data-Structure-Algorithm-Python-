2d3e25f8d60d13c5bf873b36436fccfddd7808ff
Shuang Wu <shuang.wu@emory.edu>
2022-04-16 00:25:35 -0400

    Create 976. Largest Perimeter Triangle.py

---
+++ 976. Largest Perimeter Triangle.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def largestPerimeter(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        for i in range(len(nums)-3, -1, -1):
+            if nums[i]+nums[i+1] > nums[i+2]:
+                return nums[i]+nums[i+1]+nums[i+2]
+        return 0
+        
